f0dc0d26b652f0da54919297d0d051fa7785112f
Ciara Siviter <ciara-rosina@live.co.uk>
2020-10-28 17:33:29 +0000

    adding route

--- mybookcase/src/App.js
+++ mybookcase/src/App.js
@@ -1,8 +1,11 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import React, { useState } from 'react';
 //import '../App.css';
 import Book from './components/Books';
 import data from './models/books.json';
 import Search from './components/Search';
+import Header from './components/Header';
+
 
 const App = (props) => {
 
@@ -10,6 +13,19 @@ const App = (props) => {
 
   return (
       <div>
+        <Router>
+  <Route exact path="/" render={() => (
+    <>
+      <Header />
+  <h1>This page 1</h1>
+</> )} />
+  <Route exact path="/bookcase" render={() => (
+    <>
+    <Header />
+    <h1>This is bookcase</h1>
+    
+</> )} />
+ </Router>
           {<Search/>}
           {books.map(book => <Book key={book.id} book={book}/>)}
       </div>
